182624bcf5359f5d016146b8a10825da92fccac6
Jialin Wang <jialin@everledger.io>
2023-01-23 10:55:45 +1000

    better templating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react'
 import './App.css'
+import { FooComp } from './components/CalendarComp'
 
 function App() {
-  return (
-    <div className="App">
-      Hello I'm a snipet
-    </div>
-  );
+    return (
+        <div className='App'>
+            Hello I'm a react 18 snipet
+            <FooComp />
+        </div>
+    )
 }
 
-export default App;
+export default App
